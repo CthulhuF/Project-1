8f1c7c7fe6d53de798d345627a86dc9d06cdb345
Toha1 <toha_voznuk@mail.ru>
2023-10-19 16:28:59 +0300

    zalupa

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-a = []
+list_a = []
 while 1 == 1:
     input_variable = input('Enter the next number: ')
     if input_variable == '':
         break
     else:
-        a.append(int(input_variable))
-
+        list_a.append(int(input_variable))
 
 target = int(input('Enter the target number: '))
 
-
-def sum_of_two(self, ):
-    for i in a:
-        if a[i] + a[- 1] == target:
-            print(i, len(a))
-            break
-
-    a.pop()
-    sum_of_two(a)
-
-
-sum_of_two(a)
+a = list_a
 print(a)
+
+
+i = 0
+while a:
+    if i == (len(a)-1):
+        a.pop()
+        i = 0
+
+    elif a[-1] + a[i] == target:
+        final_answer = [(len(a)-1), i]
+        print(final_answer)
+        break
+
+    else:
+        i += 1
+        print('success iteration')
+
+if not a:
+    print('no solutions')
 
