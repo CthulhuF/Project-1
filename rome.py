41793754a92d2539013f0cb95861fac5b7e3e7c4
Toha1 <toha_voznuk@mail.ru>
2023-10-24 02:52:40 +0300

    zalupa

---
+++ rome.py
@@ -0,0 +1,60 @@
+
+
+def rome_to_arabic(rome_number: str):
+    """
+    Function for translate the rome numbers to arabic
+
+    :param rome_number: str
+    :return: int
+    """
+    result = 0
+    arabic_interp = []
+    rome_equals_arabic = {
+        'I': 1,
+        'i': 1,
+        'V': 5,
+        'v': 5,
+        'X': 10,
+        'x': 10,
+        'L': 50,
+        'l': 50,
+        'C': 100,
+        'c': 100,
+        'D': 500,
+        'd': 500,
+        'm': 1000,
+        'M': 1000,
+    }
+
+    for i in range(len(rome_number)):
+        if rome_number[i] in rome_equals_arabic:
+            arabic_interp.append(rome_equals_arabic[rome_number[i]])
+        else:
+            return 'Wrong number'
+
+    for i in range(len(arabic_interp) - 1):
+        if arabic_interp[i] < arabic_interp[i + 1] and (arabic_interp[i] == 5 or arabic_interp[i] == 50):
+            return 'Wrong number'
+        elif arabic_interp[i] < arabic_interp[i + 1] and (arabic_interp[i] % 10 == 0 or arabic_interp[i] == 1):
+            arabic_interp[i] *= -1
+            i += 1
+
+
+    for i in range(len(arabic_interp)):
+        result += arabic_interp[i]
+    return result
+
+
+a = []
+temp = 0
+list_of_numbers = []
+
+while temp != '':
+    temp = input('Enter: ')
+    list_of_numbers.append(temp)
+else:
+    list_of_numbers.pop()
+
+for i in range(len(list_of_numbers)):
+    temp = list_of_numbers[i]
+    print(rome_to_arabic(temp))
