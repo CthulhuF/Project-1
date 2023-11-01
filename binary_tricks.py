6d69e1a2f24b71913c434d80915fdf7714dfac19
Toha1 <toha_voznuk@mail.ru>
2023-11-02 02:48:20 +0300

    binary

---
+++ binary_tricks.py
@@ -0,0 +1,17 @@
+a = (str(bin(int(input('enter something ')))))
+a = list(a)
+print(a)
+a.pop(0)
+a.pop(0)
+a.append('b')
+a.append('0')
+a.reverse()
+a = int((''.join(a)), 2)
+print(a)
+
+
+#print(a)
+
+
+
+
